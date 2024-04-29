dfee5783cdf519936a1dc86d9036f65c224b45fc
Danish Sayed <danishsayed.ae@gmail.com>
2024-04-29 20:44:19 +0400

    Created login page object

--- src/main/java/GenericKeywords/WebElementsInteractions.java
+++ src/main/java/GenericKeywords/WebElementsInteractions.java
@@ -1,5 +1,24 @@
 package GenericKeywords;
 
-public class WebElementsInteractions {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class WebElementsInteractions {
+    protected WebDriver driver;
+
+//These are custom created function that helps us to build keyword driven strategy
+    public void clickElement (By locator)
+    {
+        driver.findElement(locator).click();
+    }
+
+    public void sendText (By locator, String text)
+    {
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    public void goToApplication (String url)
+    {
+        driver.get(url);
+    }
 }
--- src/main/java/PageObjects/LoginPageObject.java
+++ src/main/java/PageObjects/LoginPageObject.java
@@ -1,19 +1,25 @@
 package PageObjects;
 
+import GenericKeywords.WebElementsInteractions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class LoginPageObject {
-    WebDriver driver;
+public class LoginPageObject extends WebElementsInteractions
+{
+//    WebDriver driver;
     private final By userNameTextField = By.id("user-name");
     private final By passwordTextFiled = By.id("password");
     private final By loginButton = By.id("login-button");
 
     public void userLogin(String username, String password)
     {
-        driver.findElement(userNameTextField).sendKeys(username);
-        driver.findElement(passwordTextFiled).sendKeys(password);
-        driver.findElement(loginButton).click();
+//        driver.findElement(userNameTextField).sendKeys(username);
+//        driver.findElement(passwordTextFiled).sendKeys(password);
+//        driver.findElement(loginButton).click();
+        goToApplication("https://www.saucedemo.com/");
+        sendText(userNameTextField, username);
+        sendText(passwordTextFiled, password);
+        clickElement(loginButton);
     }
 
 }
