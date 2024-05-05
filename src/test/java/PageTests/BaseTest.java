b284e8b7516bcbfcef19acd3e1c79b9eaa3f868a
Danish Sayed <danishsayed.ae@gmail.com>
2024-05-05 10:14:10 +0400

    Update BaseTest.java

--- src/test/java/PageTests/BaseTest.java
+++ src/test/java/PageTests/BaseTest.java
@@ -1,8 +1,11 @@
 package PageTests;
 
 import Base.AppConstants;
-import PageObjects.LoginPageObject;
-import PageObjects.ProductPageObject;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,10 +14,17 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
+import org.testng.internal.TestListenerHelper;
+
+import java.time.LocalDateTime;
+
+import static Utilities.ExtentReportHelper.getReport;
 
 public class BaseTest {
     protected WebDriver driver;
@@ -22,12 +32,16 @@ public class BaseTest {
     private ChromeOptions co;
     private FirefoxOptions fo;
     private EdgeOptions eo;
+//    It helps us to create independent thread
+    protected static ThreadLocal<ExtentTest> testLogger = new ThreadLocal<>();
+    protected static final ExtentReports extentReports = getReport();
+
 
     @Parameters({"browserName"})
 
 
     @BeforeTest
-    public void setupTest(@Optional String browserName)
+    public void setupTest(@Optional String browserName, ITestResult iTestResult)
     {
         if (browserName!=null)
         {
@@ -70,11 +84,23 @@ public class BaseTest {
         {
             System.out.println("Browsername is not supported!!!");
         }
+        ExtentTest extentTest = extentReports.createTest(iTestResult.getMethod().getMethodName());
+        testLogger.set(extentTest);
+        testLogger.get().log(Status.INFO, "Driver start time"+ LocalDateTime.now());
     }
 
     @AfterTest
-    public void tearDownTest()
+    public void tearDownTest(ITestResult iTestResult)
     {
+        if (iTestResult.isSuccess())
+        {
+            testLogger.get().log(Status.PASS, MarkupHelper.createLabel(iTestResult.getMethod().getMethodName()+ " is successfully passed", ExtentColor.GREEN));
+        }
+        else
+        {
+            testLogger.get().log(Status.FAIL, "Test is failed due to: "+iTestResult.getThrowable());
+            String 
+        }
         driver.quit();
     }
 }
