4da43523b7d5caf22c412b23e2d07d6d6a63240f
Danish Sayed <danishsayed.ae@gmail.com>
2024-05-10 22:22:36 +0400

    Updated AppConstant

--- src/main/java/Base/AppConstants.java
+++ src/main/java/Base/AppConstants.java
@@ -2,5 +2,6 @@ package Base;
 
 public class AppConstants {
     public static final String browserName = System.getProperty("browserName", "chrome");
-    public static final String platform = System.getProperty("platform", "remote_git");
+//    public static final String platform = System.getProperty("platform", "remote_git");
+    public static final String platform = System.getProperty("platform", "local");
 }
