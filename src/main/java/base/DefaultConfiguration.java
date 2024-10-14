102378b5c33735565ac5e377768c0f150fcc0b4a
Danish Sayed <danishsayed.ae@gmail.com>
2024-10-14 12:02:58 +0400

    Update DefaultConfiguration.java

--- src/main/java/base/DefaultConfiguration.java
+++ src/main/java/base/DefaultConfiguration.java
@@ -1,8 +1,10 @@
 package base;
 
 public class DefaultConfiguration {
-//    It will set Chrome as the default browser
+//    It will set "Chrome" as the default browser
     public static final String browserName = System.getProperty("browserName", "chrome");
 //    public static final String platform = System.getProperty("platform", "remote_git");
+
+//    It will set "Local" as the default environment
     public static final String platform = System.getProperty("platform", "local");
 }
