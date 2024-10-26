490de39a57691610433d9905b85a5d4bb75e2745
Danish Sayed <danishsayed.ae@gmail.com>
2024-10-26 14:28:44 +0400

    Updated configuration

--- src/main/java/configuration/DefaultConfiguration.java
+++ src/main/java/configuration/DefaultConfiguration.java
@@ -5,13 +5,13 @@ public class DefaultConfiguration {
     public static final String browserName = System.getProperty("browserName", "chrome");
 
 //    It will set "Local" as the default environment for test execution
-//    public static final String environment = System.getProperty("platform", "local");
+    public static final String environment = System.getProperty("platform", "local");
 
 //    It will be used to set "Docker Standalone" as the default environment for test execution
 //    public static final String environment = System.getProperty("platform", "docker-compose-standalone");
 
     //    It will be used to set "Docker Grid" as the default environment for test execution
-    public static final String environment = System.getProperty("platform", "docker-compose-grid");
+//    public static final String environment = System.getProperty("platform", "docker-compose-grid");
 
     //    It will be used to set "Jenkins" as the default environment for test execution
 //    public static final String environment = System.getProperty("platform", "jenkins");
